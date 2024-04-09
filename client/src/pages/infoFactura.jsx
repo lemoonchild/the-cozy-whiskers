eb30ef14498b51f07e74afc98c64e01248fb76ba
lemoonchild <ncastrom21@gmail.com>
2024-04-08 21:08:50 -0600

    fix: fixed some input problems in the info factura page

--- client/src/pages/infoFactura.jsx
+++ client/src/pages/infoFactura.jsx
@@ -2,12 +2,23 @@ import React, { useState, useEffect, useRef } from 'react'
 import { Link } from 'react-router-dom'
 
 import './infoFactura.css'
+import Input from '../components/input'
 import Button from '../components/button'
 
-const InfoFactura = () => {
+import SelectInput from '../components/selectInput'
+
+const InformacionFactura = () => {
   //Colocar el nombre de empleado y rol según el usuario
   const empleadoNombre = 'Nicolas Bethancourt'
   const rolEmpleado = 'Mesero'
+
+  const numeroMesa = 12
+
+  const payOptions = [
+    { value: 'efectivo', label: 'Efectivo' },
+    { value: 'tarjera', label: 'Tarjeta' },
+    { value: 'ambos', label: 'Ambos' },
+  ]
 
   // Reloj
   const [currentTime, setCurrentTime] = useState(new Date())
@@ -21,7 +32,7 @@ const InfoFactura = () => {
   }, [])
 
   return (
-    <div className="infoFactura">
+    <div className="informacionFactura">
       <div className="header">
         <img src="../resources/mainlogo.png" alt="Logo" className="main__logo" />
         <div className="employee-info">
@@ -34,48 +45,79 @@ const InfoFactura = () => {
         </div>
       </div>
       <div className="header-title">
-        <p className="main__title">¡Hola de nuevo!</p>
+        <p className="main__title">
+          {' '}
+          <span>Informacion de factura para mesa #</span> {numeroMesa}
+        </p>
         <p className="current-time">{currentTime.toLocaleTimeString()}</p>
       </div>
 
-      <form className="table__form">
-        <Input //INFORMACIÓN DE USUARIO
-          className="table__input"
-          label="Ingresar Mesa"
-          type="text"
-          name="table"
-          id="table"
-          placeholder="Introduce el número de mesa"
-          isNumeric={true}
-        />
-        <Input //INFORMACIÓN DE CONTRASEÑA
-          className="table__input"
-          label="Personas en la mesa"
-          type="people-amount"
-          name="people-amount"
-          id="people-amount"
-          placeholder="Introduce cuantas personas se encuentran en la mesa"
-          isNumeric={true}
-        />
-        <div className="login__button-container">
-          <Link to={'/ordenMesero'}>
+      <form className="factura__form">
+        <div className="factura__column1">
+          <div className="facturainput">
+            <Input //INFORMACIÓN NIT
+              className="table__input"
+              label="Ingrese el NIT:"
+              type="text"
+              name="table"
+              id="table"
+              placeholder="Introduce el NIT"
+              isNumeric={true}
+            />
+          </div>
+          <div className="facturainput">
+            <Input //INFORMACIÓN NIT
+              className="table__input"
+              label="Ingrese el nombre:"
+              type="text"
+              name="table"
+              id="table"
+              placeholder="Introduce el nombre"
+              isNumeric={false}
+            />
+          </div>
+        </div>
+        <div className="factura__column2">
+          <div className="facturainput">
+            <Input //INFORMACIÓN NIT
+              className="table__input"
+              label="Ingrese la ciudad:"
+              type="text"
+              name="table"
+              id="table"
+              placeholder="Introduce la ciudad"
+              isNumeric={false}
+            />
+          </div>
+          <div className="facturainput">
+            <SelectInput
+              className="table__input"
+              label="Tipo de pago:"
+              name="role"
+              id="role"
+              options={payOptions}
+            />
+          </div>
+        </div>
+        <div className="factura__button-container">
+          <Link to={'/factura'}>
             <Button
-              text="Abrir cuenta"
+              text="Imprimir factura"
               onClick={() => console.log('Cuenta abierta exitosamente')}
             />
           </Link>
         </div>
       </form>
 
-      <div className="footer">
+      <div className="footer_factura">
         <p className="page__message">Your Second Home, with a Feline Twist</p>
       </div>
     </div>
   )
 }
 
-const InfoFacturas = ({ action }) => {
-  return <div>{action === 'infoFactura' && <InfoFactura />}</div>
+const InformaciondeFactura = ({ action }) => {
+  return <div>{action === 'informacionFactura' && <InformacionFactura />}</div>
 }
 
-export default InfoFacturas
+export default InformaciondeFactura
